5f824d1a1fe809330c60f4200e7c5ce59f88f7af
khalil <khalil.elwaer@gmail.com>
2023-02-11 15:04:05 +0100

    JSX

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Styles.css';
+function Address() {
+    return (
+        <>
+        
+        <textarea id="text" name="text" rows="4" value={"Cité El Khadhra, Rue les martyrs \nAgareb 3030 \nSfax - Tunisie"} disabled ></textarea> 
+
+        </>
+    )
+}
+
+export default Address;
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FullName(props) {
+    return (
+        <>
+         <p>{props.name}</p>
+         
+        </>
+    )
+}
+
+export default FullName;
